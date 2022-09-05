c7fa693da528e5e3d8deff70845028d8942258f0
Darius Garcia <dariusgarcia888@gmail.com>
2022-09-04 21:39:56 -0700

    added github links to project source code

--- components/singleWebsite.js
+++ components/singleWebsite.js
@@ -1,13 +1,15 @@
 import { useState } from 'react'
+import Link from 'next/link'
 import Image from 'next/image'
 import { GrExpand } from 'react-icons/gr'
-import { BsArrowsAngleExpand } from 'react-icons/bs'
+import { BsArrowsAngleExpand, BsGithub } from 'react-icons/bs'
 
 export default function SingleWebsite({
 	name,
 	description,
 	tech,
 	image,
+	link,
 	type,
 }) {
 	const [open, setOpen] = useState(false)
@@ -19,23 +21,46 @@ export default function SingleWebsite({
 			}}
 			id={name}
 			key={name}
-			className='relative mb-4 md:mb-0 min-h-96 h-max p-4 border-2 hover:border-blue3 text-white border-darkGrey rounded-md transition  ease-in-out delay-110 shadow-xl'>
+			className='relative mb-4 md:mb-0 min-h-96 h-max p-4 border-[1px] hover:border-blue3 text-white border-darkGrey rounded-md transition hover:bg-opacity-20 ease-in-out delay-110 shadow-xl'>
 			{!open ? (
 				<div className=''>
-					<div className='flex flex-row justify-between'>
-						<h3 className='font-bold'> {name}</h3>
+					<div className='flex flex-row px-2 justify-between'>
+						<div className='flex flex-row items-center gap-2'>
+							<h3 className='text-xl font-bold'> {name}</h3>
+							<Link href={link}>
+								<a></a>
+							</Link>
+							<BsGithub
+								className='hover:scale-125 ease-in-out transition delay-75 hover:text-blue3'
+								size={20}
+							/>
+						</div>
 						<p className='hover:scale-125 transition ease-in-out delay-25 cursor-pointer text-white'>
 							<BsArrowsAngleExpand
 								className='hover:text-blue3'
 								size='20'></BsArrowsAngleExpand>
 						</p>
 					</div>
-					<p className='mt-2 text-darkGrey'>{description}</p>
+					<p className='mt-2 px-2 text-darkGrey'>{description}</p>
+					<button className=' text-blue3 hover:bg-blue3 hover:bg-opacity-40 rounded-lg p-2'>
+						<Link href={link}>
+							<a target='_blank'>Link to {name}</a>
+						</Link>
+					</button>
 				</div>
 			) : (
 				<div className=' top-20 flex flex-col cursor-text '>
 					<div className='flex flex-row justify-between'>
-						<h3 className='font-bold text-blue3'> {name}</h3>
+						<div className='flex flex-row items-center gap-2'>
+							<h3 className='text-xl font-bold'> {name}</h3>
+							<Link href={link}>
+								<a></a>
+							</Link>
+							<BsGithub
+								className='cursor-pointer hover:scale-125 ease-in-out transition delay-75 hover:text-blue3'
+								size={20}
+							/>
+						</div>
 
 						<p className='hover:scale-75 transition ease-in-out delay-25 cursor-pointer'>
 							<BsArrowsAngleExpand
@@ -43,10 +68,14 @@ export default function SingleWebsite({
 								size='20'></BsArrowsAngleExpand>
 						</p>
 					</div>
-					<p className='mt-2'>Tech stack: {tech}</p>
-					<p className='mt-2 text-sm text-darkGrey'>{description}</p>
-
-					<div className='flex justify-center items-center cursor-pointer mt-4 '>
+					<p className='ml-2 mt-2'>Tech stack: {tech}</p>
+					<p className='mt-2 ml-2 text-sm text-darkGrey'>{description}</p>
+					<button className='w-max text-blue3 hover:bg-blue3 hover:bg-opacity-40 rounded-lg p-2'>
+						<Link href={link}>
+							<a target='_blank'>Link to {name}</a>
+						</Link>
+					</button>
+					<div className='flex justify-center items-center cursor-pointer mt-12 '>
 						<video
 							autoPlay
 							muted
--- pages/projects.js
+++ pages/projects.js
@@ -15,7 +15,6 @@ import instapic from '../public/videos/instapic.gif'
 import flixter from '../public/videos/flixter.gif'
 
 const Projects = () => {
-	// const [isOpen, setIsOpen] = useState([])
 	const [websiteOpen, setWebsiteOpen] = useState(true)
 	const [mobileOpen, setMobileOpen] = useState(false)
 	const handleStatusChange = (status) => {
@@ -29,8 +28,10 @@ const Projects = () => {
 			<main className='flex justify-center mx-4 lg:mx-0 md:pt-12 mb-12 bg-black2 '>
 				<div className='md:grid md:grid-cols-3 w-full lg:w-5/6'>
 					<div>
-						<div className='md:ml-4 md:fixed px-8 pb-8 row-span-1 md:mt-4 flex flex-col items-center border-white md:border-[1px]  md:bg-opacity-20 md:mr-8 h-max  rounded-lg'>
-							<h1 className='mb-4 md:mt-4 md:mb-8 text-3xl'>Projects</h1>
+						<div className='md:fixed px-8 pb-8 row-span-1 md:mt-4 flex flex-col items-center border-darkGrey md:border-[1px]  md:bg-opacity-20 md:mr-8 h-max  rounded-lg'>
+							<h1 className='mb-4 mt-4 md:mb-8 text-3xl font-semibold'>
+								Projects
+							</h1>
 							<div className='flex flex-col items-start gap-8'>
 								<button
 									onClick={() => {
@@ -44,10 +45,10 @@ const Projects = () => {
 									}}
 									className={
 										!websiteOpen
-											? 'bg-darkGrey w-full font-semibold p-4 rounded-lg transition ease-in-out delay-55 hover:scale-95 hover:opacity-75'
-											: 'bg-blue3 w-full font-semibold p-4 rounded-lg transition ease-in-out delay-55 hover:scale-95 hover:opacity-75'
+											? 'bg-darkGrey text-sm w-full font-semibold p-4 rounded-lg transition ease-in-out delay-55 hover:scale-95 hover:opacity-75'
+											: 'bg-blue3 text-sm w-full font-semibold p-4 rounded-lg transition ease-in-out delay-55 hover:scale-95 hover:opacity-75'
 									}>
-									websites & web apps
+									WEBSITES & WEB APPS
 								</button>
 								<button
 									onClick={() => {
@@ -61,10 +62,10 @@ const Projects = () => {
 									}}
 									className={
 										!mobileOpen
-											? 'bg-darkGrey w-full font-semibold p-4 rounded-lg transition ease-in-out delay-55 hover:scale-95 hover:opacity-75 '
-											: 'bg-blue3 w-full font-semibold p-4 rounded-lg transition ease-in-out delay-55 hover:scale-95 hover:opacity-75 '
+											? 'bg-darkGrey text-sm w-full font-semibold p-4 rounded-lg transition ease-in-out delay-55 hover:scale-95 hover:opacity-75 '
+											: 'bg-blue3 text-sm w-full font-semibold p-4 rounded-lg transition ease-in-out delay-55 hover:scale-95 hover:opacity-75 '
 									}>
-									mobile apps
+									MOBILE APPS
 								</button>
 							</div>
 						</div>
@@ -72,7 +73,10 @@ const Projects = () => {
 							<h1 className=''></h1>
 						</div>
 					</div>
-					<div className=' row-span-2 col-span-2 overflow-auto'>
+
+					{/* 2nd column of the grid container */}
+					{/* containers the individual projects */}
+					<div className=' row-span-2 col-span-2 overflow-auto md:mr-2'>
 						<header className='mb-8 md:mb-12'>
 							<p className='mt-4 md:text-lg '>
 								Here are some of the recent projects I have completed.
@@ -80,8 +84,8 @@ const Projects = () => {
 						</header>
 						{websiteOpen ? (
 							<section className=''>
-								<h2 className=' text-xl md:text-2xl mb-4'>
-									Websites and Web Apps
+								<h2 className=' text-xl font-bold md:text-2xl mb-4'>
+									WEBSITES AND WEB APPS
 								</h2>
 								<div className='flex flex-col md:gap-8 justify-between cursor-pointer mb-24'>
 									{projects.websites.map((project) => (
@@ -91,6 +95,7 @@ const Projects = () => {
 											description={project.description}
 											tech={project.tech}
 											image={project.image}
+											link={project.link}
 											type={project.type}
 											setOpen={handleStatusChange}
 										/>
@@ -99,8 +104,10 @@ const Projects = () => {
 							</section>
 						) : (
 							<section className=''>
-								<h2 className='text-xl md:text-2xl mb-4'>Mobile Apps</h2>
-								<div className='flex flex-col  md:gap-8 justify-between cursor-pointer mb-24'>
+								<h2 className='text-xl md:text-2xl mb-4 font-bold'>
+									MOBILE APPS
+								</h2>
+								<div className='flex flex-col md:gap-8 justify-between cursor-pointer mb-24'>
 									{projects.webapps.map((project) => (
 										<SingleMobileApp
 											key={project.name}
@@ -109,7 +116,7 @@ const Projects = () => {
 											tech={project.tech}
 											image={project.image}
 											type={project.type}
-											// setOpen={handleStatusChange}
+											link={project.link}
 										/>
 									))}
 								</div>
@@ -132,6 +139,7 @@ const projects = {
 				"Freelance Next.js project for a client that lets customers send payments utilizing Stripe's payment API.",
 			tech: 'Next.js, React, Tailwind CSS',
 			image: fixit,
+			link: 'https://fititcredit.com/',
 			type: 'video/mp4',
 		},
 		{
@@ -140,6 +148,7 @@ const projects = {
 				'Full-stack MERN web app that lets users add stocks from the S&P500 index to their own personal watch list.',
 			tech: 'React, Tailwind CSS, node.js, REST APIs, user Auth',
 			image: bullbear,
+			link: 'https://bullbearmarket.net/',
 			type: 'video/mp4',
 		},
 		{
@@ -148,6 +157,7 @@ const projects = {
 				"Freelance marketing website for a small business that's built with Next.js.",
 			tech: 'Next.js, React, Tailwind CSS',
 			image: kvl,
+			link: 'https://kvl-communications.com/',
 			type: 'video/mp4',
 		},
 	],
@@ -159,6 +169,7 @@ const projects = {
 				'Full-stack Android mobile app that lets users login to their Twitter account and compose new Tweets.',
 			tech: 'Kotlin, REST APIs, user Auth',
 			image: tweeter,
+			link: 'https://github.com/dariusgarcia/tweeter',
 		},
 		{
 			name: 'Instapic',
@@ -166,6 +177,7 @@ const projects = {
 				'Full-stack Android mobile photo-sharing app similar to Instagram.',
 			tech: 'Kotlin, REST APIs, user Auth',
 			image: instapic,
+			link: 'https://github.com/dariusgarcia/instapic',
 		},
 		{
 			name: 'Flixter',
@@ -173,6 +185,7 @@ const projects = {
 				'Android mobile app that allows users to browse movies from the The Movie Database API.',
 			tech: 'Kotlin, REST APIs, user Auth',
 			image: flixter,
+			link: 'https://github.com/dariusgarcia/flixter',
 		},
 	],
 }
