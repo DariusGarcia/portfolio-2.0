78522694f416c31a8a895d2902e59735faa7ae70
Darius Garcia <dariusgarcia888@gmail.com>
2022-09-05 16:01:50 -0700

    changed skills page to grid layout

--- pages/skills.js
+++ pages/skills.js
@@ -11,13 +11,13 @@ const Skills = () => {
 			</Head>
 			<NavBar />
 			<main className='py-12 flex flex-col items-center  bg-black2'>
-				<div className='px-6 flex flex-col md:w-5/6'>
-					<h1 className='text-3xl font-semibold mb-8'>Skills</h1>
-					<p className=''>
-						This is my current tech stack that I use to develop my applications:
-					</p>
+				<div className='px-6 flex flex-col md:w-5/6 mb-8'>
+					<h1 className='text-4xl font-semibold mb-4'>Skills</h1>
+					<h2 className='text-lg font-semibold'>
+						Current tech stack that I use to develop my web applications:
+					</h2>
 				</div>
-				<section className='md:w-5/6 md:px-6 grid grid-cols-2 md:grid-cols-4 gap-8 mt-12'>
+				<section className='md:w-5/6 md:px-6 grid grid-cols-2 md:grid-cols-4 gap-8 '>
 					{skills.current.map((skill) => (
 						<article
 							className='flex justify-center  items-center p-8 font-bold bg-blue3 bg-opacity-20 border-2 border-blue3 text-blue3 rounded-xl selection:bg-darkGrey selection:text-black hover:scale-105 transition ease-in-out delay-100 hover:bg-opacity-100 hover:text-white'
@@ -27,11 +27,11 @@ const Skills = () => {
 					))}
 				</section>
 				<div className='px-6 flex flex-col md:w-5/6 mt-12'>
-					<h1 className='text-3xl font-semibold mb-8'>
-						Other technologies I have worked with in the past
-					</h1>
+					<h2 className='text-lg font-semibold mb-8'>
+						Other technologies I have worked with in the past:
+					</h2>
 				</div>
-				<section className='md:w-5/6 md:px-6 flex gap-8'>
+				<section className='md:w-5/6 md:px-6 grid grid-cols-2  gap-8'>
 					{skills.previous.map((skill) => (
 						<article
 							className='flex justify-center items-center p-8 font-bold bg-blue3 bg-opacity-20 border-2 border-blue3 text-blue3 rounded-xl selection:bg-darkGrey selection:text-black hover:scale-105 transition ease-in-out delay-100 hover:bg-opacity-100 hover:text-white'
@@ -39,6 +39,11 @@ const Skills = () => {
 							{skill}
 						</article>
 					))}
+					<article
+						className='col-span-2 flex justify-center items-center p-8 font-bold bg-blue3 bg-opacity-20 border-2 border-blue3 text-blue3 rounded-xl selection:bg-darkGrey selection:text-black hover:scale-105 transition ease-in-out delay-100 hover:bg-opacity-100 hover:text-white'
+						key='android'>
+						Android Mobile Development
+					</article>
 				</section>
 			</main>
 			<Footer />
@@ -62,7 +67,7 @@ const skills = {
 		'REST APIs',
 	],
 
-	previous: ['Python', 'Kotlin', 'Android Mobile Development'],
+	previous: ['Python', 'Kotlin'],
 }
 
 export default Skills
