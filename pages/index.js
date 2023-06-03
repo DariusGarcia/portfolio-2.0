c5bea13db4b34b5a1f5145f1631d170d28c4a79d
Darius Garcia <dariusgarcia888@gmail.com>
2023-06-02 17:44:50 -0700

    update animation to spring

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,24 @@ import Navbar from '../components/navbar'
 import Footer from '../components/footer'
 import ContactBtn from '../components/contactBtn'
 import PageWrapper from '../components/framer-motion/page-wrapper'
+import { motion as m, AnimatePresence } from 'framer-motion'
 
 export default function Landing() {
+  const cardVariants = {
+    hidden: {
+      y: 300,
+      opacity: 0,
+    },
+    visible: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        type: 'spring',
+        bounce: 0.5,
+        duration: 1,
+      },
+    },
+  }
   return (
     <>
       <Head>
@@ -14,40 +30,48 @@ export default function Landing() {
       <ContactBtn />
       <div className='bg-black2 text-white'>
         <Navbar />
-        <PageWrapper>
-          <div className='relative isolate px-6 md:pt-56 lg:px-8 bg-black2 h-screen'>
-            <div
-              className='absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80'
-              aria-hidden='true'
-            ></div>
-            <div className='mx-auto max-w-2xl py-32 lg:py-0'>
-              <div className='text-center'>
-                <h1 className='text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl'>
-                  Darius Garcia Jr
-                </h1>
-                <p className='mt-6 text-lg leading-8 text-gray-600'>
-                  I'm an aspiring frontend developer. I currently work as a
-                  coding instructor for Code Ninjas. I enjoy making fully
-                  responsive and elegant web apps.
-                </p>
-                <div className='mt-10 flex items-center justify-center gap-x-6'>
-                  <Link
-                    href='/projects'
-                    className='rounded-md px-3.5 py-2.5 text-sm font-semibold text-white  shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
-                  >
-                    Projects
-                  </Link>
-                  <Link
-                    href='/experience'
-                    className='text-sm font-semibold leading-6 '
-                  >
-                    Experience →
-                  </Link>
+        {/* <PageWrapper> */}
+        <AnimatePresence>
+          <m.div
+            initial='hidden'
+            animate='visible'
+            viewport={{ once: true, amount: 0.8 }}
+            className='relative isolate px-6 md:pt-56 lg:px-8 bg-black2 h-screen'
+          >
+            <m.div variants={cardVariants}>
+              <div
+                className='absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80'
+                aria-hidden='true'
+              ></div>
+              <div className='mx-auto max-w-2xl py-32 lg:py-0'>
+                <div className='text-center'>
+                  <h1 className='text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl'>
+                    Darius Garcia Jr
+                  </h1>
+                  <p className='mt-6 text-lg leading-8 text-gray-600'>
+                    I'm an aspiring frontend developer. I currently work as a
+                    coding instructor for Code Ninjas. I enjoy making fully
+                    responsive and elegant web apps.
+                  </p>
+                  <div className='mt-10 flex items-center justify-center gap-x-6'>
+                    <Link
+                      href='/projects'
+                      className='rounded-md px-3.5 py-2.5 text-sm font-semibold text-white  shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
+                    >
+                      Projects
+                    </Link>
+                    <Link
+                      href='/experience'
+                      className='text-sm font-semibold leading-6 '
+                    >
+                      Experience →
+                    </Link>
+                  </div>
                 </div>
               </div>
-            </div>
-          </div>
-        </PageWrapper>
+            </m.div>
+          </m.div>
+        </AnimatePresence>
       </div>
       <Footer />
     </>
